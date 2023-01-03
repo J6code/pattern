6db7b35b59be57f65f0e1da20c5bae8002e4318b
hj.luo <hj.luo@sibionics.com>
2023-01-03 17:42:46 +0800

    upd(command):算法模式-demo1 注释调整。

--- pure/src/command/demo1/main/RemoteControlTest.java
+++ pure/src/command/demo1/main/RemoteControlTest.java
@@ -3,9 +3,6 @@ package command.demo1.main;
 import command.demo1.service.impl.Light;
 import command.demo1.service.impl.LightOnCommand;
 
-/**
- * 命令模式:将“请求”封装成对象，以便使用不同的请求、队列或者日志来参数化其他对象。命令模式也支持可撤销的操作。
- */
 public class RemoteControlTest {
     public static void main(String[] args) {
         // 遥控器-算法调用者
--- pure/src/command/demo1/main/doRun.java
+++ pure/src/command/demo1/main/doRun.java
@@ -3,6 +3,9 @@ package command.demo1.main;
 import command.demo1.service.Command;
 import command.demo1.service.impl.*;
 
+/**
+ * 命令模式:将“请求”封装成对象，以便使用不同的请求、队列或者日志来参数化其他对象。命令模式也支持可撤销的操作。
+ */
 public class doRun {
     // 组合算法
     public static void main(String[] args) {
